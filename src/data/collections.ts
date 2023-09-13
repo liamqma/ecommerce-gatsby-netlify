644d341933cbd4bca3c7627be13784a8b5eae854
Liam Ma <liam.q.ma@gmail.com>
2023-09-13 22:19:12 +1000

    Add images

--- src/data/collections.ts
+++ src/data/collections.ts
@@ -7,22 +7,22 @@ export type Collection = {
 
 const c: Collection[] = [
   {
-    id: 'collection-1',
-    name: 'Collection 1',
-    image: '/static/1.jpg',
-    products: [
-      'nkyo1bq0u8odob',
-      'nhwdaxbjj6lgw5',
-      'nhwdaxbjj6lgw1',
-      'nhwdaxbjj6lgw2',
-      'nhwdaxbjj6lgw3',
-    ],
+    id: 'flower-frames',
+    name: 'Flower Frames',
+    image: '/static/mysa-12.jpg',
+    products: ['od3nvr7zgxrmqy', 'od3lsemuayzi3z'],
   },
   {
-    id: 'collection-2',
-    name: 'Collection 2',
-    image: '/static/2.jpg',
-    products: ['nkyo1bq0u8odob', 'nhwdaxbjj6lgw5'],
+    id: 'handtied-posies',
+    name: 'Handtied Posies',
+    image: '/static/mysa-03.jpg',
+    products: ['od3g6cuu9sx4j6', 'od3fj37dkkppln', 'od3c33cfrb7mfj', 'od3yclricys82l'],
+  },
+  {
+    id: 'flower-id-frames',
+    name: 'Flower ID Frames',
+    image: '/static/mysa-64.jpg',
+    products: ['od44omns7egb3e', 'od40wba88lnihy'],
   },
 ];
 
