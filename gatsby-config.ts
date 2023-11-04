9aa2189d28c3018ab513477cec57c7ddca0df884
Liam Ma <liam.q.ma@gmail.com>
2023-11-05 10:48:33 +1100

    Add Google Analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -21,5 +21,21 @@ module.exports = {
     },
     'gatsby-plugin-emotion',
     'gatsby-plugin-netlify',
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-TDCHXHBJ3S', // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
   ],
 };
